91c577b6fee3117e2c6cf4b90e8e0e1768104592
Johnny Wellington <johnny@arbeitstudio.com.br>
2025-02-15 10:12:43 -0300

    Fix map for single source object

--- pom/mapper.py
+++ pom/mapper.py
@@ -1,49 +1,60 @@
 from __future__ import annotations
-from collections import defaultdict, ChainMap
+
+from collections import ChainMap, defaultdict
 from collections.abc import Iterable
-from inspect import getmembers
 from functools import partial
-from typing import TypeVar, Type, Union, Tuple
+from inspect import getmembers
+from typing import Callable, Mapping, Tuple, Type, TypeVar, Union
 
 TS = TypeVar("TS")
 TT = TypeVar("TT")
 
-SourceType = Union[Type[TS], Tuple[Type[TS]]]
+SourceType = Union[Type[TS], Tuple[Type[TS], ...]]
 TargetType = Type[TT]
 Source = Union[TS, Tuple[TS]]
+
 
 def prop():
     def prop(x):
         return x
+
     return prop
 
 
 class Mapper:
     def __init__(self):
-        self.mappings = defaultdict(
-            partial(defaultdict, partial(defaultdict, prop))
-        )
+        self.mappings = defaultdict(partial(defaultdict, partial(defaultdict, prop)))
 
     def add_mapping(self, *, source: SourceType, target: TargetType, mapping=None):
         self.mappings[source][target].update(mapping or {})
 
-    def map(self, source: TS, target_type: TargetType, skip_init=True) -> TT:
+    def map(self, source: TS, target_type: TargetType, skip_init=False) -> TargetType:
         if isinstance(source, Iterable):
             props = ChainMap(*[dict(self._get_props(so)) for so in source])
             maps = self.mappings[tuple(type(so) for so in source)][target_type]
         else:
             props = ChainMap(dict(self._get_props(source)))
-            maps = self.mappings[source][target_type]
+            maps = self.mappings[type(source)][target_type]
         mapped = self._maps(maps, props)
-        if skip_init:
-            instance = target_type.__new__(target_type)
-            for prop in mapped:
-                setattr(instance, prop[0], prop[1])
-        return instance
-        return target_type(**{k: v for k, v in mapped})
+        try:
+            if skip_init:
+                instance = object.__new__(target_type)
+                for prop in mapped:
+                    setattr(instance, prop[0], prop[1])
+                return instance
+            return target_type(**{k: v for k, v in mapped})
+        except TypeError as e:
+            if isinstance(source, Iterable):
+                raise TypeError(
+                    f"Source objects ({', '.join([type(so).__name__ for so in source])}) are missing required properties for target object {target_type.__name__}: {e}"
+                )
+
+            raise TypeError(
+                f"Source object {type(source).__name__} is missing required properties for target object {target_type.__name__}: {e}"
+            )
 
     def _get_props(self, obj) -> list[tuple]:
         return [prop for prop in getmembers(obj) if not prop[0].startswith("_")]
 
-    def _maps(self, maps: dict[callable], props: dict) -> list[tuple]:
+    def _maps(self, maps: dict[str, Callable], props: Mapping) -> list[tuple]:
         return list(map(lambda p: (p[0], maps[p[0]](p[1])), props.items()))
--- pom/test_mapper.py
+++ pom/test_mapper.py
@@ -1,4 +1,30 @@
+from contextlib import nullcontext as does_not_raise
+
+import pytest
+
 from pom import Mapper
+
+
+@pytest.mark.parametrize(
+    "skip_init, check_if_raised",
+    [(True, does_not_raise()), (False, pytest.raises(TypeError))],
+)
+def test_map_source_object_missing_property(skip_init, check_if_raised):
+    class A:
+        def __init__(self, email: str):
+            self.email = email
+
+    class B:
+        def __init__(self, name: str, email: str):
+            self.name = name
+            self.email = email
+
+    a = A("johnny@mail.com")
+
+    mapper = Mapper()
+    mapper.add_mapping(source=A, target=B, mapping={"name": lambda n: n[::-1]})
+    with check_if_raised:
+        b = mapper.map(a, B, skip_init=skip_init)
 
 
 def test_map():
@@ -8,18 +34,17 @@ def test_map():
             self.email = email
 
     class B:
-        def __init__(self, name: str, email:str):
+        def __init__(self, name: str, email: str):
             self.name = name
             self.email = email
 
-    a = A('Johnny', 'johnny@mail.com')
+    a = A("Johnny", "johnny@mail.com")
 
     mapper = Mapper()
-    mapper.add_mapping(
-        source=A, 
-        target=B, 
-        mapping={'name': lambda n: n[::-1]})
+    mapper.add_mapping(source=A, target=B, mapping={"name": lambda n: n[::-1]})
     b = mapper.map(a, B)
+    assert isinstance(b, B)
+    assert b.name == "ynnhoJ"
 
 
 def test_map_aggregate():
@@ -30,29 +55,58 @@ def test_map_aggregate():
             super().__init__()
 
     class B:
-        def __init__(self, name: str, email:str, age: str):
+        def __init__(self, name: str, email: str, age: str):
             self.name = name
             self.email = email
             self.age = age
             super().__init__()
-    
+
     class C:
         def __init__(self, name: str, email: str, age: str):
             self.name = name
-            self.email  = email
+            self.email = email
             self.age = age
             super().__init__()
 
-    a = A('Johnny', 'johnny@mail.com')
-    b = B('Jodin', 'johnyblaw@blawcloud.com', 30)
+    a = A("Johnny", "johnny@mail.com")
+    b = B("Jodin", "johnyblaw@blawcloud.com", 30)
 
     mapper = Mapper()
-    mapper.add_mapping(
-        source=(A,B), 
-        target=C, 
-        mapping={'name': lambda n: n[::-1]}
-    )
-    c = mapper.map((a,b), C)
+    mapper.add_mapping(source=(A, B), target=C, mapping={"name": lambda n: n[::-1]})
+    c = mapper.map((a, b), C)
+    assert isinstance(c, C)
     assert c.name == "ynnhoJ"
     assert c.email == a.email
     assert c.age == b.age
+
+
+@pytest.mark.parametrize(
+    "skip_init, check_if_raised",
+    [(True, does_not_raise()), (False, pytest.raises(TypeError))],
+)
+def test_map_aggregate_missing_property(skip_init, check_if_raised):
+    class A:
+        def __init__(self, email: str):
+            self.email = email
+            super().__init__()
+
+    class B:
+        def __init__(self, email: str, age: int):
+            self.email = email
+            self.age = age
+            super().__init__()
+
+    class C:
+        def __init__(self, name: str, email: str, age: int):
+            self.name = name
+            self.email = email
+            self.age = age
+            super().__init__()
+
+    a = A("johnny@mail.com")
+    b = B("johnyblaw@blawcloud.com", 30)
+
+    mapper = Mapper()
+    mapper.add_mapping(source=(A, B), target=C, mapping={"name": lambda n: n[::-1]})
+    with check_if_raised:
+        c = mapper.map((a, b), C, skip_init=skip_init)
